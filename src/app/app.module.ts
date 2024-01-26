836e385002077f2ac55178cf5ae8bd006c489a53
Grzegorz Bączek <Gregor333000@gmail.com>
2024-01-26 19:46:21 +0100

    add framework primeNG ang simple button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,17 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ButtonModule } from 'primeng/button'
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    ButtonModule,
     AppRoutingModule
   ],
   providers: [
